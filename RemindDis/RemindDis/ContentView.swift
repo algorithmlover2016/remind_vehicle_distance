e87855e54eda03628f30b686ad93b2de8a07a0d6
algorithmlover2016 <yubo.upt@gmail.com>
2023-05-31 16:44:56 +0800

    add speed display successfully in ios

--- RemindDis/RemindDis/ContentView.swift
+++ RemindDis/RemindDis/ContentView.swift
@@ -11,9 +11,9 @@ import MapKit
 
 class LocationManager: NSObject, CLLocationManagerDelegate, ObservableObject {
     private let locationManager = CLLocationManager()
-    
+
     @Published var location: CLLocation? = nil
-    
+
     override init() {
         super.init()
         self.locationManager.delegate = self
@@ -21,33 +21,44 @@ class LocationManager: NSObject, CLLocationManagerDelegate, ObservableObject {
         self.locationManager.requestWhenInUseAuthorization()
         self.locationManager.startUpdatingLocation()
     }
-    
+
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let location = locations.last else { return }
         self.location = location
     }
-    
+
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print("Error getting location: \(error.localizedDescription)")
     }
 }
 
 struct ContentView: View {
-    let locationManager = LocationManager()
-    
+    @ObservedObject var locationManager = LocationManager()
+    @State private var speed: CLLocationSpeed = 0.0
+
     var body: some View {
-        VStack(spacing: 12.0) {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-                .font(.largeTitle)
-                .fontWeight(.bold)
+        VStack {
+            HStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Text("Hello, world!")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(.blue)
+            }
+            .padding()
+            .background(Color.yellow)
+            .cornerRadius(10)
+
+            Text("Current Speed: \(speed, specifier: "%.2f") m/s")
+                .font(.headline)
                 .foregroundColor(.blue)
                 .padding()
                 .background(Color.yellow)
                 .cornerRadius(10)
-            MapView(locationManager: locationManager)
+
+            MapView(locationManager: locationManager, speed: $speed)
                 .frame(height: 300)
                 .cornerRadius(10)
         }
@@ -57,53 +68,73 @@ struct ContentView: View {
 
 struct MapView: UIViewRepresentable {
     @ObservedObject var locationManager: LocationManager
-    @State private var speed: CLLocationSpeed = 0.0
-    
+    @Binding var speed: CLLocationSpeed
+
     func makeUIView(context: Context) -> MKMapView {
         let mapView = MKMapView()
         mapView.showsUserLocation = true
         return mapView
     }
-    
+
     func updateUIView(_ uiView: MKMapView, context: Context) {
         guard let location = locationManager.location else { return }
         let region = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)
         uiView.setRegion(region, animated: true)
-        
+
         // Add pin annotation to the map view
         let annotation = MKPointAnnotation()
         annotation.coordinate = location.coordinate
         annotation.title = "Current Location"
         uiView.addAnnotation(annotation)
-        
+
         // Update speed
         speed = location.speed
     }
-    
+
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
-    
+
     class Coordinator: NSObject, MKMapViewDelegate {
         var parent: MapView
-        
+
         init(_ parent: MapView) {
             self.parent = parent
         }
-        
+
         func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-            let identifier = "pin"
+            let identifier = "pos"
             var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
             if annotationView == nil {
-                annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
                 annotationView?.canShowCallout = true
+
+                // Customize the pin annotation
+                let imageView = UIImageView(image: UIImage(systemName: "location.fill"))
+                imageView.tintColor = .red
+                annotationView?.leftCalloutAccessoryView = imageView
+
+                let button = UIButton(type: .detailDisclosure)
+                annotationView?.rightCalloutAccessoryView = button
             } else {
                 annotationView?.annotation = annotation
             }
             return annotationView
         }
     }
+    // Return a Text view from the body property
+    /*
+    @ViewBuilder
+    var body: some View {
+        VStack {
+            Text("Speed: \(speed, specifier: "%.2f") m/s")
+                .font(.headline)
+                .foregroundColor(.blue)
+        }
+    }
+    */
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
